ef388764bdc67b23a6559dd0e220d23001c1abfb
bcatek <kiranbcatek@gmail.com>
2026-01-09 00:24:53 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+ // TIMEZONE (VERY IMPORTANT)
+date_default_timezone_set('Asia/Kolkata');
+define('BOT_TOKEN', '8169957313:AAF1tAwFVbOzpTT9dtK1OM30UO9R_JCA11g');
+define('DB_HOST', 'sql110.infinityfree.com');
+define('DB_USER', 'if0_40730636');
+define('DB_PASS', 'Kini1991');
+define('DB_NAME', 'if0_40730636_nixo28');
+
+?>
